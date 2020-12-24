5838de91d0e02fa9c5c23beb1322c0be5391d3e2
Adarsh Srivastava <herecomesthebeastlesnar@gmail.com>
2020-12-24 12:43:56 +0530

    Monster Game Done

--- Project 2/app.js
+++ Project 2/app.js
@@ -3,15 +3,50 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
-      confirmedName: ""
+      confirmedName: "",
+      // fullname: "",
+      lastName: "",
     };
   },
+  watch: {
+    // name(value) {
+    //   if (value === "") this.fullname = "";
+    //   else this.fullname = value + " " + this.lastName;
+    // },
+    // lastName(value) {
+    //   if (value === "") this.fullname = "";
+    //   else this.fullname = this.name + " " + value;
+    // },
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+  },
+  computed: {
+    fullname() {
+      console.log("Running agin!!");
+      if (this.name === "" || this.lastName === "") return "";
+      return this.name + " " + this.lastName;
+    },
+  },
   methods: {
+    outputFullName() {
+      console.log("Running agin!!");
+      if (this.name === "") return "";
+      return this.name + " " + "Srivastava";
+    },
     confirmInput() {
-        this.confirmedName = this.name
+      this.confirmedName = this.name;
     },
     submitForm() {
-        alert("Submitted!!");
+      alert("Submitted!!");
+    },
+    resetInput() {
+      this.name = "";
     },
     add(num) {
       this.counter = this.counter + num;
@@ -21,7 +56,7 @@ const app = Vue.createApp({
       this.counter = this.counter - num;
     },
     setName(event, lastName) {
-      this.name = event.target.value + " " + lastName;
+      this.name = event.target.value;
     },
   },
 });
