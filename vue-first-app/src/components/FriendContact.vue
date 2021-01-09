20cf9a4e963c43c0bde57b6235d315552aad44d0
adarshSrivastava01 <59641575+adarshSrivastava01@users.noreply.github.com>
2021-01-09 19:23:02 +0530

    Props and Component Communication

--- vue-first-app/src/components/FriendContact.vue
+++ vue-first-app/src/components/FriendContact.vue
@@ -1,30 +1,55 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">Show Details</button>
     <ul v-if="detailsAreVisible">
-      <li><strong>Phone:</strong>{{ friend.phone }}</li>
-      <li><strong>Phone:</strong>{{ friend.email }}</li>
+      <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+      <li><strong>Phone:</strong>{{ emailAddress }}</li>
     </ul>
   </li>
 </template>
 
 <script>
 export default {
+  //   props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  props: {
+    id: {
+      type: String,
+      rqeuired: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      //   validator: function (value) {
+      //     return value === "0" || value === "1";
+      //   },
+    },
+  },
   data() {
     return {
-      detailsAreVisible: false,
-      friend: {
-        id: "manuel",
-        name: "Manuel Lorenz",
-        phone: "0123 45678 90",
-        email: "manuel@localhost.com",
-      },
+      detailsAreVisible: false
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
     },
   },
 };
